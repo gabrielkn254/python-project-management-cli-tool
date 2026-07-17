2ece8065332abf3b3fb9cc0186f8deecd9d1e48d
Gabriel <gabrielkn254@gmail.com>
2026-07-17 12:21:51 +0300

    Models: add @classmethods to classes

--- models/project.py
+++ models/project.py
@@ -1,10 +1,11 @@
+from models.task import Task
 class Project:
-    def __init__(self, title, description, due_date):
+    def __init__(self, title, description, due_date, user_id):
         self.title = title
         self.description = description
         self.due_date = due_date
         self.tasks = []
-        self.owner = None
+        self.owner = user_id
     
     def __repr__(self):
         return self.title
@@ -14,8 +15,20 @@ class Project:
             "title": self.title,
             "description": self.description,
             "due_date": self.due_date,
-            "tasks": self.tasks
+            "tasks": self.tasks,
+            "owner": self.owner
         }
+    
+    @classmethod
+    def from_dict(cls, data: dict) -> "Project":
+        tasks = [Task.from_dict(t) for t in data.get("tasks", [])]
+        return cls(
+            title=data["title"],
+            description=data["description"],
+            due_date=data["due_date"],
+            tasks=tasks,
+            owner=data["owner"]            
+        )
     
     def add_task(self, task):
         self.tasks.append(task)
